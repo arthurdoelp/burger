21d0d7e02a4aa3edfdb4cc489559aac69db7fe7a
arthurdoelp <arthurdoelp@gmail.com>
2019-04-20 16:20:12 -0400

    Initial push including full MVC format and preliminary html functioning and reading from the SQL

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bison Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lamb Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pineapple Burger', true);
